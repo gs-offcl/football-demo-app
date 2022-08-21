a4888959436e4405585a20620a6c5380e8adb7da
91973 <91973@host.docker.internal>
2022-08-22 01:33:42 +0530

    angular demo

--- src/app/services/football.service.ts
+++ src/app/services/football.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 import { TeamStanding } from '../classes/team.standing';
 
 @Injectable({
@@ -14,11 +14,14 @@ export class FootballService {
   getData(teamStanding : TeamStanding)
   {
     let params = new HttpParams();
+    
+    let headers= new HttpHeaders()
+    .set('X-API-KEY', 'suresh123');
 
     params = params.append('teamName', teamStanding.team);
     params = params.append('countryName', teamStanding.country);
     params = params.append('leagueName', teamStanding.league);
     
-    return this.httpClient.get(this.baseUrl,  { params: params });
+    return this.httpClient.get(this.baseUrl,  { params: params, headers:  headers });
   }
 }
